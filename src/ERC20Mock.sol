dc8ec475a41331da0a7c55dc890be55e74a9d6fd
Isaac <teryima127@gmail.com>
2023-10-12 19:25:31 +0100

    refactored

--- src/ERC20Mock.sol
+++ src/ERC20Mock.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.13;
 import "solmate/tokens/ERC20.sol";
 
 contract FractionalNFT is ERC20 {
-    constructor() ERC20("FRACTIONAL_NFT", "FRN", 18) {
-        // _mint(msg.sender, 1000000 * 10 ** 18); // Mint 1,000,000 fractionNFT tokens to the contract deployer
-    }
+    constructor() ERC20("FRACTIONAL_NFT", "FRN", 18) {}
 
     function mint(address account, uint256 amount) external {
         _mint(account, amount);
